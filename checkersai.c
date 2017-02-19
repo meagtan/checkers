5be519e36f5265285f4ed59f66def26ef0ba7a92
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-02-19 12:43:44 +0300

    Corrected player-checking error

--- checkersai.c
+++ checkersai.c
@@ -10,19 +10,19 @@ int play_optimal_move(struct checkers *c)
 move optimal_move(struct checkers *c)
 {
     move best;
-    alphabeta(c, MAX_DEPTH, INT_MIN, INT_MAX, c->turn, &best);
+    alphabeta(c, MAX_DEPTH, INT_MIN, INT_MAX, c->player, &best);
     return best;
 }
 
 int alphabeta(struct checkers *c, int depth, int alpha, int beta, int player, move *best)
 {
     if (!depth || c->scores[0] == 12 || c->scores[1] == 12)
-        return c->scores[player] - c->scores[!player];
+        return 2 * c->scores[player] - c->scores[!player]; // favors scoring more
     int v, v1, ptc;
     move pts[MAX_MOVES], m;
     struct checkers c1;
     
-    v = player == c->turn ? INT_MIN : INT_MAX;
+    v = player == c->player ? INT_MIN : INT_MAX;
     
     // iterate through each valid (row, col) that can move
     ptc = moves(c, pts);
@@ -31,11 +31,11 @@ int alphabeta(struct checkers *c, int depth, int alpha, int beta, int player, mo
         !checkers_play(&c1, MOVE_ROW(pts[i]), MOVE_COL(pts[i]), MOVE_DIR(pts[i]));
         v1 = alphabeta(&c1, depth - 1, alpha, beta, player, &m);
         
-        if ((player == c->turn ? v < v1 : v > v1) || ((rand() & 1) && v == v1)) { // randomized tie breaking
+        if ((player == c->player ? v < v1 : v > v1) || ((rand() & 1) && v == v1)) { // randomized tie breaking
             v = v1;
             *best = pts[i];
         }
-        if (player == c->turn ? alpha < v : alpha > v) 
+        if (player == c->player ? alpha < v : alpha > v) 
             alpha = v;
         if (alpha >= beta)
             break;
